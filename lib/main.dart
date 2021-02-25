3ec7785c4446d8bc7f906537d2b7f1f3f642dda9
Soksedtha Ly <soksedtha.ly@student.mahidol.edu>
2021-02-26 01:37:31 +0700

    Fixed oversized page

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/',
-  routes: {
-    '/': (context) => MyStore(),
-    '/book': (context) => BookPage(),
-    '/payment': (context) => PaymentPage(),
-  },
-));
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MyStore(),
+        '/book': (context) => BookPage(),
+        '/payment': (context) => PaymentPage(),
+      },
+    ));
 
 class Book {
   String url;
@@ -80,115 +80,117 @@ class MyStore extends StatefulWidget {
 }
 
 class _MyStoreState extends State<MyStore> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'My Book Store',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 22,
+      appBar: AppBar(
+        title: Text(
+          'My Book Store',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
+        ),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: ListView.builder(
+        itemCount: books.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 1,
+              horizontal: 4,
             ),
-          ),
-          backgroundColor: Colors.blue[900],
-        ),
-        body: ListView.builder(
-          itemCount: books.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 1,
-                horizontal: 4,
-              ),
-              child: Card(
-                child: ListTile(
-                  leading: ClipRRect(
-                    child: Image.asset(books[index].url),
-                  ),
-                  title: Text(books[index].title),
-                  subtitle: Text('Price: THB ${books[index].price}'),
-                  trailing: Icon(Icons.add),
-                  onTap: () {
-                    currentIndex = index;
-                    Navigator.pushNamed(context, '/book').then((value) => setState(() {}));
-                  },
+            child: Card(
+              child: ListTile(
+                leading: ClipRRect(
+                  child: Image.asset(books[index].url),
                 ),
+                title: Text(books[index].title),
+                subtitle: Text('Price: THB ${books[index].price}'),
+                trailing: Icon(Icons.add),
+                onTap: () {
+                  currentIndex = index;
+                  Navigator.pushNamed(context, '/book')
+                      .then((value) => setState(() {}));
+                },
               ),
-            );
-          },
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: Container(
+        height: 80,
+        // color: Colors.blue[900],
+        decoration: BoxDecoration(
+          color: Colors.blue[900],
+          boxShadow: [
+            BoxShadow(color: Colors.grey),
+          ],
         ),
-        bottomNavigationBar: Container(
-          height: 80,
-          // color: Colors.blue[900],
-          decoration: BoxDecoration(
-            color: Colors.blue[900],
-            boxShadow: [
-              BoxShadow(color: Colors.grey),
-            ],
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  SizedBox(width: 30),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Total Price:',
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.8),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14,
-                        ),
-                      ),
-                      Text(
-                        'THB $totalPrice',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 26,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue[800],
-                      padding: EdgeInsets.all(20),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                SizedBox(width: 30),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Total Price:',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
                       ),
                     ),
-                    child: Text(
-                      'Check Out',
+                    Text(
+                      'THB $totalPrice',
                       style: TextStyle(
                         color: Colors.white,
                         fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                        fontSize: 26,
                       ),
                     ),
-                    onPressed: () {
-                      setState(() {
-                        if(totalPrice>0) Navigator.pushNamed(context, '/payment').then((value) => setState(() {}));
-                      });
-                    },
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue[800],
+                    padding: EdgeInsets.all(20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                  SizedBox(width: 15),
-                ],
-              ),
-            ],
-          ),
+                  child: Text(
+                    'Check Out',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      if (totalPrice > 0)
+                        Navigator.pushNamed(context, '/payment')
+                            .then((value) => setState(() {}));
+                    });
+                  },
+                ),
+                SizedBox(width: 15),
+              ],
+            ),
+          ],
         ),
+      ),
     );
   }
 }
@@ -202,148 +204,148 @@ class _BookPageState extends State<BookPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Details of ${books[currentIndex].title}',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
-            ),
+      appBar: AppBar(
+        title: Text(
+          'Details of ${books[currentIndex].title}',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
           ),
-          backgroundColor: Colors.blue[900],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Image.asset(
-                books[currentIndex].url,
-                scale: 0.4,
-                height: 300,
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image.asset(
+              books[currentIndex].url,
+              scale: 0.4,
+              height: 300,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                books[currentIndex].title,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32,
+                ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  books[currentIndex].title,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 32,
+            ),
+            // SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 100.0,
+                // vertical: 10,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    'Price:',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
                   ),
+                  Text(
+                    'THB ${books[currentIndex].price}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(45.0),
+              child: Text(
+                'BEST SELLER',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32,
                 ),
               ),
-              // SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 100.0,
-                  // vertical: 10,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 80,
+        // color: Colors.blue[900],
+        decoration: BoxDecoration(
+          color: Colors.blue[900],
+          boxShadow: [
+            BoxShadow(color: Colors.grey),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                SizedBox(width: 30),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      'Price:',
+                      'Total Price:',
                       style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
                         fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                        fontSize: 14,
                       ),
                     ),
                     Text(
-                      'THB ${books[currentIndex].price}',
+                      'THB $totalPrice',
                       style: TextStyle(
+                        color: Colors.white,
                         fontWeight: FontWeight.bold,
-                        fontSize: 24,
+                        fontSize: 26,
                       ),
                     ),
                   ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(70.0),
-                child: Text(
-                  'BEST SELLER',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 32,
+              ],
+            ),
+            Row(
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue[800],
+                    padding: EdgeInsets.all(20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
+                  child: Text(
+                    'Add to Cart',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      totalPrice += books[currentIndex].price;
+                      cart.add(books[currentIndex]);
+                      // Navigator.of(context).pushNamed('/').then((value) => setState(() {}));
+                      Navigator.pop(context);
+                    });
+                  },
                 ),
-              ),
-            ],
-          ),
+                SizedBox(width: 15),
+              ],
+            ),
+          ],
         ),
-        bottomNavigationBar: Container(
-          height: 80,
-          // color: Colors.blue[900],
-          decoration: BoxDecoration(
-            color: Colors.blue[900],
-            boxShadow: [
-              BoxShadow(color: Colors.grey),
-            ],
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  SizedBox(width: 30),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Total Price:',
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.8),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14,
-                        ),
-                      ),
-                      Text(
-                        'THB $totalPrice',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 26,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue[800],
-                      padding: EdgeInsets.all(20),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    child: Text(
-                      'Add to Cart',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                      ),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        totalPrice += books[currentIndex].price;
-                        cart.add(books[currentIndex]);
-                        // Navigator.of(context).pushNamed('/').then((value) => setState(() {}));
-                        Navigator.pop(context);
-                      });
-                    },
-                  ),
-                  SizedBox(width: 15),
-                ],
-              ),
-            ],
-          ),
-        ),
+      ),
     );
   }
 }
